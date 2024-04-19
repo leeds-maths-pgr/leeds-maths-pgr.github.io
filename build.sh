9c6af76e9c0336da5fb7ad24d09d798e8055a247
Luca Seemungal <luca@lseem.xyz>
2024-04-19 21:55:51 +0100

    Try out a wee trick

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-rm -rf site/* \
-	&& mkdir -p site \
-	&& mkdir -p site/pure-seminar \
+rm -rf docs/* \
+	&& mkdir -p docs \
+	&& mkdir -p docs/pure-seminar \
 	&& echo "Folder structure creation successful."
-pandoc -s content/index.md -o site/index.html \
-	&& pandoc -s content/pure-seminar.md -o site/pure-seminar/index.html \
+pandoc -s content/index.md -o docs/index.html \
+	&& pandoc -s content/pure-seminar.md -o docs/pure-seminar/index.html \
 	&& echo "Site generation successful."
