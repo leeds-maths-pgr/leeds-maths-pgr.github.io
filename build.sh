b4cdc4c7d9904ad8f33f2112d760e85c33cc2720
Gautam Chaudhuri <gautam.chaudhuri.1803@gmail.com>
2024-04-22 14:24:22 +0100

    rewrite build script
    - rewrite the build script so pages are not hard coded into it
    - update GH actions to reflect changes

--- build.sh
+++ build.sh
@@ -1,12 +1,61 @@
-#!/bin/sh
-rm -rf docs/* \
-	&& mkdir -p www \
-	&& mkdir -p www/pure \
-	&& mkdir -p www/applied \
-	&& mkdir -p www/statistics \
-	&& echo "Folder structure creation successful."
-pandoc -s content/index.md -o www/index.html \
-	&& pandoc -s content/pure/index.md -o www/pure/index.html \
-	&& echo "Site generation successful."
-	#&& pandoc -s content/applied.md -o www/applied/index.html \
-	#&& pandoc -s content/statistics.md -o www/statistics/index.html \
+#!/usr/bin/env sh
+
+source "./utility.sh"
+
+# Globals
+
+DIR_CONT="content"
+DIR_OUTP=
+LST_FILE=
+
+
+# Options
+if [ $# -eq 0 ]; then
+        echo "ERR: No options provided"
+        echo "(run $0 -h for help)"
+        echo ""
+        exit 1
+fi
+
+oflag=
+cflag=
+
+while getopts "o:ch" OPTION; do
+        case $OPTION in
+
+                o)
+                        oflag="true"
+                        DIR_OUTP="$OPTARG";;
+
+                c)
+                        cflag="true";;
+
+                h)
+                        echo "Usage:"
+                        echo "args.sh -h "
+                        echo "args.sh [-c] -o OUTPDIR "
+                        echo ""
+                        echo "   -o OUTPDIR      Run the script writing the generated output to OUTPDIR"
+                        echo "   -c              Do not build the site and just exit after cleaning the output directory"
+                        echo "   -h              help (this output)"
+                        exit 0
+                        ;;
+
+        esac
+done
+
+if [ -z "$oflag" ]; then
+    echo "ERR: Output directory not specified.\nPlease specify an output directory using the flag '-o OUTPDIR'"
+    exit 1
+fi
+
+# Generic sed command that needs to be passed to other functions
+SUBSTITUTION="s|${DIR_CONT}|${DIR_OUTP}|"
+
+_clean "${DIR_OUTP}"
+if [ ! -z "$cflag" ]; then
+    exit 0
+fi
+LST_FILE="$(collect_files ${DIR_CONT})"
+_make_dirs "$LST_FILE" "$SUBSTITUTION"
+_render_site "${LST_FILE}" "$SUBSTITUTION"
---
+++ utility.sh
@@ -0,0 +1,70 @@
+# Pure functions
+
+str_add () {
+    # Usage: str_add A B
+    # Returns: "A:B" if A does not contain B, else returns A
+    if [ -z "$1" ]; then
+        printf "$2"
+    elif ! printf "$1" | /bin/grep -E -q "(^|:)${2}($|:)"; then
+        printf "${1}:${2}"
+    else
+        printf "${1}"
+    fi
+}
+
+collect_files () {
+    # Usage: collect_files CONTENT_DIR
+    # Return a ':' separated list of files in CONTENT DIR to process
+    # Assumes that a markdown file is to be processed iff it is named 'index.md'
+    outp=
+    for f_name in $(find "$1" -mindepth 1 -type f -name '*index.md'); do
+        outp=$(str_add "$outp" "$f_name")
+    done
+    printf "$outp"
+}
+
+get_out_path () {
+    # Usage: get_out_path FILE SUBSTITUTION
+    # Returns the output path of FILE according to SUBSTITUTION
+    printf "$1" | sed -E "$2;s|md$|html|"
+}
+
+# Non-pure functions
+
+_make_dirs () {
+    # Usage: _make_dirs FILE_LIST SUBSTITUTION
+    # Given a list of files, create the corresponding directories in the output directory
+    OIFS="$IFS"
+    IFS=":"
+    for f_name in $1; do
+        d_name=$(dirname $f_name | sed -E "$2")
+        mkdir -p "${d_name}"
+    done
+    IFS="$OIFS"
+}
+
+_clean () {
+    # Usage: _clean DIR
+    # Remove all files in DIR
+    find "$1" -mindepth 1 -delete
+}
+
+_render_file () {
+    # Usage: _render FILE SUBSTITUTION
+    # Renders FILE as HTML
+    # Assumes that FILE exists and the corresponding output directories have been created
+
+    outfile="$(get_out_path $1 $2)"
+    pandoc -s --mathml "$1" -o "$outfile"
+}
+
+_render_site () {
+    # Usage: _render_site FILE_LIST SUBSTITUTION
+    # Given a list of files, render each of them
+    OIFS="$IFS"
+    IFS=":"
+    for f_name in $1; do
+        _render_file "${f_name}" "$2"
+    done
+    IFS="$OIFS"
+}
